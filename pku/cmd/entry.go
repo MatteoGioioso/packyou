68fe1f8f46879219719357625e16161c55763103
MatteoGioioso <gioioso.matteo@gmail.com>
2021-01-29 09:26:25 +0700

    refactor(): move everything under application service
    extract common method into a domain object

--- pku/cmd/entry.go
+++ pku/cmd/entry.go
@@ -2,13 +2,96 @@ package cmd
 
 import (
 	"github.com/spf13/cobra"
-	"packyou/pku/fileCollector"
+	"log"
+	"packyou/pku/compiler"
+	"packyou/pku/errorPkg"
+	"packyou/pku/fileRepository"
+	"packyou/pku/pathResolver"
+	"path/filepath"
+	"strings"
+)
+
+var (
+	fileRepo fileRepository.FileRepository
+	pathRes pathResolver.PathResolver
+	comp compiler.Compiler
 )
 
 func initializeCommand(cmd *cobra.Command, getConfig func(key string) interface{}) {
 	entry := cmd.Flag("entry").Value.String()
 	projectRoot := cmd.Flag("projectRoot").Value.String()
 	output := cmd.Flag("output").Value.String()
+	fileRepo = fileRepository.New()
+	pathRes = pathResolver.New(projectRoot, entry, output)
+	comp = compiler.New(pathRes)
 
-	fileCollector.New(entry, projectRoot, output).Collect()
+	entryFileLocation := filepath.Join(projectRoot, entry)
+	if err := collect(entryFileLocation); err != nil {
+		log.Fatal(err)
+	}
+}
+
+
+func collect(originFilePath string) error {
+	file, err := fileRepo.GetFile(originFilePath)
+	if err != nil {
+		return errorPkg.New(err, "collect")
+	}
+
+	lines := strings.Split(string(file), "\n")
+	for _, line := range lines {
+		if pathRes.IsES6Module(line) {
+			if err := parseES6Module(line, originFilePath); err != nil {
+				return errorPkg.New(err, "collect")
+			}
+		}
+
+		if pathRes.IsCommonJs(line) {
+			// Implement commonJs
+		}
+	}
+
+	newFile := fileRepo.RewriteToFile(string(file))
+	destFileLocation, err := pathRes.GetDestFileLocation(originFilePath)
+	if err != nil {
+		return errorPkg.New(err, "collect")
+	}
+
+	if err := fileRepo.SaveFile(destFileLocation, []byte(newFile)); err != nil {
+		return errorPkg.New(err, "collect")
+	}
+
+	return nil
+}
+
+func parseES6Module(line, currentOriginFilePath string) error {
+	var importPath string
+	if pathRes.IsUnnamedES6Import(line) {
+		// TODO: handle un-named imports "import babel/regenerator"
+		// no-op
+		return nil
+	}
+
+	importPath = pathRes.ExtractImportPathFromLine(line)
+	if pathRes.IsNodeModule(importPath) {
+		// TODO: for now just copy node_modules into the dest folder
+		// no-op
+		return nil
+	}
+
+	originFileLocation := pathRes.GetOriginFileLocation(currentOriginFilePath, importPath)
+	destFileLocation, err := pathRes.GetDestFileLocation(originFileLocation)
+	if err != nil {
+		return errorPkg.New(err, "parseES6Module")
+	}
+
+	if err := fileRepo.MoveFileToDest(originFileLocation, destFileLocation); err != nil {
+		return errorPkg.New(err, "parseES6Module")
+	}
+
+	newLine, newImportPath := pathRes.ChangeMovedFileImportPath(line, importPath)
+	newLine = comp.TransformImport(newLine, newImportPath)
+	fileRepo.AddRewrite(line, newLine)
+
+	return collect(originFileLocation)
 }
---
+++ pku/fileRepository/fileRepository.go
@@ -0,0 +1,74 @@
+package fileRepository
+
+import (
+	"github.com/pkg/errors"
+	"io/ioutil"
+	"os"
+	"packyou/pku/errorPkg"
+	"path/filepath"
+	"strings"
+)
+
+type fileRepository struct {
+	rewrites map[string]string
+}
+
+func New() *fileRepository {
+	return &fileRepository{
+		rewrites: make(map[string]string, 0),
+	}
+}
+
+func (f fileRepository) SaveFile(outputPath string, file []byte) error {
+	if err := os.MkdirAll(filepath.Dir(outputPath), 0770); err != nil {
+		return errors.Wrap(err, "MkdirAll\n")
+	}
+
+	if err := ioutil.WriteFile(outputPath, file, os.ModePerm); err != nil {
+		return errors.Wrap(err, "writeFile\n")
+	}
+
+	return nil
+}
+
+func (f *fileRepository) MoveFileToDest(originFileLocation, destFileLocation string) error {
+	file, err := f.GetFile(originFileLocation)
+	if err != nil {
+		return err
+	}
+
+	if err := f.SaveFile(destFileLocation, file); err != nil {
+		return errors.Wrap(err, "saveFile\n")
+	}
+
+	return nil
+}
+
+func (f fileRepository) GetFile(importPath string) ([]byte, error) {
+	file, err := ioutil.ReadFile(importPath)
+	if err != nil {
+		return nil, errorPkg.New(err, "getFile")
+	}
+
+	return file, err
+}
+
+func (f fileRepository) RewriteToFile(file string) string {
+	for oldPath, newPath := range f.rewrites {
+		file = strings.ReplaceAll(file, oldPath, newPath)
+	}
+
+	return file
+}
+
+func (f *fileRepository) AddRewrite(line string, newLine string) {
+	f.rewrites[line] = newLine
+}
+
+type FileRepository interface {
+	SaveFile(outputPath string, file []byte) error
+	MoveFileToDest(originFileLocation, destFileLocation string) error
+	GetFile(importPath string) ([]byte, error)
+	RewriteToFile(file string) string
+	AddRewrite(line string, newLine string)
+}
